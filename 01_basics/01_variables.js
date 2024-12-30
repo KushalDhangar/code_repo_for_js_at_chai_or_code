e2da7226d057c99b2908f8b97a454955d3cf8463
Kushal Dhangar <155418668+KushalDhangar@users.noreply.github.com>
2024-12-30 08:38:10 +0000

    Conversion Operations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=1251
+let accountName="Kushal Dhangar"
+var accountEmail="aman@gmailcom "
+accountDetails = 12121
+console.log(accountEmail);
+console.table([accountId,accountName,accountEmail,accountDetails])
