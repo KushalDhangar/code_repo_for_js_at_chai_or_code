2c146f7e781e3af05f0bb7367fbc1aea090b4d02
Kushal Dhangar <155418668+KushalDhangar@users.noreply.github.com>
2024-12-30 08:16:32 +0000

    datatypes with their types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let name="Kushal ABhay"
+let age=13
+let value=null;
+let dtypes;
+console.table([name,age,value,dtypes])
+
+console.log(typeof"name")
+console.log(typeof value);
+console.log(typeof dtypes);
+
